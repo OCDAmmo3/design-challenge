206194271c92ed855e88a9b0197a3718c9e263d3
Jon Struve <ocdammo3@gmail.com>
2019-11-06 14:17:16 -0600

    Added styling to make sidebar hidden until hover

--- src/components/drawer/drawer.js
+++ src/components/drawer/drawer.js
@@ -9,5 +9,18 @@ const Drawer = props => {
 export default Drawer;
 
 Drawer.defaultProps = {
-  children: [<div>Stuff</div>],
+  children: 
+  [
+    <div className="sideBar">
+      <h3>Stuff</h3>
+      <nav>
+        <ul className="navBar">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+      </nav>
+    </div>
+  ],
 };

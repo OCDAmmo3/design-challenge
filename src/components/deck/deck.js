f71c1fff9d2dc277ecfbc89e1dea47c866362e7e
Jon Struve <ocdammo3@gmail.com>
2019-11-08 15:26:57 -0600

    Added styling for 2 column grid

--- src/components/deck/deck.js
+++ src/components/deck/deck.js
@@ -39,5 +39,32 @@ Deck.defaultProps = {
         <li>Item 5</li>
       </ul>
     </div>,
+    <div className="card">Card
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul>
+    </div>,
+    <div className="card">Card
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul>
+    </div>,
+    <div className="card">Card
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul>
+    </div>,
   ],
 };
